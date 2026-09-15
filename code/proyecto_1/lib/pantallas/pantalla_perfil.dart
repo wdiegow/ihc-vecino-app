013763dc46db220b28e47cf49f00e72c529dd015
EnriqueFrancoM <enriquecba10@gmail.com>
2026-09-15 19:48:00 -0400

    Update pantalla_perfil.dart

--- code/proyecto_1/lib/pantallas/pantalla_perfil.dart
+++ code/proyecto_1/lib/pantallas/pantalla_perfil.dart
@@ -10,47 +10,47 @@ class PantallaPerfil extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: const Text('Perfil')), backgroundColor: Colors.white,
       body: SafeArea(child: Padding(padding: const EdgeInsets.all(16.0),
-          child: Container(width: double.infinity, padding: const EdgeInsets.all(24),
+        child: Container(width: double.infinity, padding: const EdgeInsets.all(24),
 
-            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
 
-                Text('MI PERFIL', textAlign: TextAlign.center, style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 24, fontWeight: FontWeight.bold,),),
-                const SizedBox(height: 40),
+            Text('MI PERFIL', textAlign: TextAlign.center, style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 24, fontWeight: FontWeight.bold,),),
+            const SizedBox(height: 40),
 
-                Text('Nombre', style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 14,),),
-                const SizedBox(height: 8),
+            Text('Nombre', style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 14,),),
+            const SizedBox(height: 8),
 
-                Text(PerfilStore.nombre, style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 18, fontWeight: FontWeight.bold,),),
-                const SizedBox(height: 24),
+            Text(PerfilStore.nombre, style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 18, fontWeight: FontWeight.bold,),),
+            const SizedBox(height: 24),
 
-                Text('Casa', style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 14,),),
-                const SizedBox(height: 8),
+            Text('Casa', style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 14,),),
+            const SizedBox(height: 8),
 
-                Text(PerfilStore.casa, style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 18, fontWeight: FontWeight.bold,),),
-                const SizedBox(height: 64),
+            Text(PerfilStore.casa, style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 18, fontWeight: FontWeight.bold,),),
+            const SizedBox(height: 64),
 
-                //BOTON EDITAR PEFIL
-                SizedBox(
-                  width: double.infinity,
-                  child: OutlinedButton(
+            //BOTON EDITAR PEFIL
+            SizedBox(
+              width: double.infinity,
+              child: OutlinedButton(
 
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const PantallaEditarPerfil(),),);
-                    },
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const PantallaEditarPerfil(),),);
+                },
 
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade700, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),), padding: const EdgeInsets.symmetric(vertical: 16),),
-                    child: Text('Editar Perfil', style: GoogleFonts.inter(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold,),
-                    ),
-                  ),
-
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade700, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),), padding: const EdgeInsets.symmetric(vertical: 16),),
+                child: Text('Editar Perfil', style: GoogleFonts.inter(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold,),
                 ),
-
-              ],
-
+              ),
 
             ),
+
+          ],
+
+
           ),
         ),
+      ),
       ),
     );
   }
