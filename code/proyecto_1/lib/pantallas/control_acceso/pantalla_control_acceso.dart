e6d038dc7b97012e9c980a3cb57d6fc5dcd11b4c
EnriqueFrancoM <enriquecba10@gmail.com>
2026-09-18 22:30:44 -0400

    Create pantalla_control_acceso.dart

---
+++ code/proyecto_1/lib/pantallas/control_acceso/pantalla_control_acceso.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pantalla_nueva_autorizacion.dart';
+import 'pantalla_mis_visitas.dart';
+import '../perfil/pantalla_perfil.dart';
+import '../../data/perfil_store.dart';
+
+class PantallaControlAcceso extends StatelessWidget {
+  const PantallaControlAcceso({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Control de Acceso')), backgroundColor: Colors.white,
+      body: SafeArea(child: Padding(padding: const EdgeInsets.all(16.0),
+        child: Container(width: double.infinity, padding: const EdgeInsets.all(24),
+
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+            Text('CONTROL ACCESO', textAlign: TextAlign.center, style: GoogleFonts.inter(color: Color(0xFF143125), fontSize: 35, fontWeight: FontWeight.bold, letterSpacing: 2,),),
+            const SizedBox(height: 32),
+
+            Text('Hola, ${PerfilStore.nombre}', style: GoogleFonts.inter(color: Colors.black, fontSize: 16,),),
+            const SizedBox(height: 8),
+
+            Text(PerfilStore.casa, style: GoogleFonts.inter(color: Colors.black, fontSize: 16,),),
+            const SizedBox(height: 24),
+
+            Text('2 visitas esperadas', style: GoogleFonts.inter(color: Colors.black, fontSize: 14,),),
+            const SizedBox(height: 32),
+
+            //BOTON NUEVA AUT
+            SizedBox(
+              width: double.infinity, child: OutlinedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const PantallaNuevaAutorizacion(),),);
+                },
+
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade700, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),), padding: const EdgeInsets.symmetric(vertical: 16),),
+              child: Text('+ Nueva autorización', style: GoogleFonts.inter(color: Colors.white, fontSize: 14,),),
+
+              ),
+            ),
+            const SizedBox(height: 40),
+
+            //MENU MIS VISITAS
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const PantallaMisVisitas(),),);
+                },
+
+                child: Text('Mis visitas', style: GoogleFonts.inter(color: Colors.black, fontSize: 14,),),
+
+              ),
+
+            //MENU PERFIL
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const PantallaPerfil(),),);
+                },
+
+                child: Text('Perfil', style: GoogleFonts.inter(color: Colors.black, fontSize: 14,),),
+              ),
+
+            ],
+            ),
+          ],
+          ),
+        ),
+      ),
+      ),
+    );
+  }
+}
