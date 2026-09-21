6b109b9d828f368092fc1a2a492d545970e63313
EnriqueFrancoM <enriquecba10@gmail.com>
2026-09-21 15:50:07 -0400

    Update perfil_store.dart

--- code/proyecto_1/lib/data/perfil_store.dart
+++ code/proyecto_1/lib/data/perfil_store.dart
@@ -3,12 +3,18 @@ class PerfilStore {
 
   static String nombre = 'Mateo';
   static String casa = 'Casa #24';
+  static String correo = 'mateo@gmail.com';
+  static String celular = '77893712';
 
   static void actualizarPerfil({
     required String nuevoNombre,
     required String nuevaCasa,
+    required String nuevoCorreo,
+    required String nuevoCelular,
   }) {
     nombre = nuevoNombre;
     casa = nuevaCasa;
+    correo = nuevoCorreo;
+    celular = nuevoCelular;
   }
 }
